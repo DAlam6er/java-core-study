20734b9a94feb7baef49000ee9bb67b19970c86e
Anton Pashkin <anton.zep@gmail.com>
2022-11-27 15:11:00 +0300

    moved jdbc study examples to another project

--- jdbc_library/src/com/dmdev/jdbc/starter/exception/DaoException.java
+++
@@ -1,9 +0,0 @@
-package com.dmdev.jdbc.starter.exception;
-
-public class DaoException extends RuntimeException
-{
-    public DaoException(Throwable throwable)
-    {
-        super(throwable);
-    }
-}
--- jdbc_library/src/com/personsDB/sedykh/App.java
+++
@@ -1,9 +0,0 @@
-package com.personsDB.sedykh;
-
-public class App
-{
-    public static void main(String[] args)
-    {
-        new GUI().buildGUI();
-    }
-}
